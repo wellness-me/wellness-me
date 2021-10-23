bbac47e392b4e013c3bf11d431bd91cb771f219d
Colin Curtis <colinpcurtis826@ucla.edu>
2021-10-22 23:42:54 -0700

    add logging

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,8 @@
 const express = require("express")
-const mongoose = require('mongoose');
-require('dotenv').config()
-
-const routes = require('./routes/v1');
-
 const cors = require('cors');
 
+const routes = require('./routes/v1');
 const app = express()
-
-// use dev when testing
-const db_type = process.env.USE_DEV ? "wellness-dev" : "wellness-prod"
-mongoose.connect(process.env.MONGO_URL + db_type)
 
 app.use(cors({
     origin: '*'
--- backend/controllers/userdata.controller.js
+++ backend/controllers/userdata.controller.js
@@ -1,10 +1,13 @@
 const httpStatus = require('http-status');
 const { UserData } = require("../models/userData.model")
+const logger = require("../utils/logger")
 
 const createData = async (req, res) => {
     const data = new UserData(req.body)
 
     const result = await UserData.create(data)
+    logger.debug("created userdata")
+    
     res.status(httpStatus.CREATED).send(result)
 }
 
