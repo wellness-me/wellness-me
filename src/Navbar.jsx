a5ec24a94fa1725a2685f8fa14c0ec073a98cd11
Elvis Li <runqianli@icloud.com>
2021-10-30 18:31:03 -0700

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+<Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Wellness-me</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
