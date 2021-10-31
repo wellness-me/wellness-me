bf57d1c155ad7fd878ebcac48e7077d5a2a06c1d
Colin Curtis <colinpcurtis826@ucla.edu>
2021-10-30 22:50:17 -0700

    add comment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Input, Button } from 'semantic-ui-react'
 
 const Form = () => {
+    // todo: make this only accessible if we've logged in
     const [sleep, setSleep] = useState("")
     const [exercise, setExercise] = useState("")
 
