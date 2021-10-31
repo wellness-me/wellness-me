e7a6f175786be81fa899471e7635efc836084581
Colin Curtis <colinpcurtis826@ucla.edu>
2021-10-30 18:14:54 -0700

    add working frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'semantic-ui-css/semantic.min.css'
 import Form from './components/Form';
 
 function App() {
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
+import { Input } from 'semantic-ui-react'
+
 const Form = (props) => {
     const [sleep, setSleep] = useState("")
-    
+    const [exercise, setExercise] = useState("")
+
+    const postToAPI = async () => {
+        const data = {
+            "sleep": sleep,
+            "exercise": exercise
+        }
+
+        await fetch("http://localhost:5000/v1/data/", {
+            method: "POST",
+            body: data,
+        })
+    }
+
 
     return (
         <div>
-            <input type="text" />
+            <div>
+                <Input focus placeholder='Sleep' onChange={(e) => setSleep(e.target.value) } />
+            </div>
+
+            <div>
+                <Input focus placeholder='Happiness' onChange={(e) => setExercise(e.target.value) } />
+            </div>
 
         </div>
     )
