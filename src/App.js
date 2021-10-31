f5d147584aaebd37a762d4c0fe36a394f55b981d
Elvis Li <runqianli@icloud.com>
2021-10-30 20:34:37 -0700

    Add Navbar.jsx
    Added Navbar.jsx. It can not do anything yet and items are set to be A B C D for now.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Form from './components/Form';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header">
       </header> */}
+      <Navbar />
       <Form />
 
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Input, Menu } from 'semantic-ui-react'
+
+const Navbar = (props) => {
+  return (
+    <div class="ui four item menu">
+      <a class="active item">A</a>
+      <a class="item">B</a>
+      <a class="item">C</a>
+      <a class="item">D</a>
+    </div>
+  )
+}
+
+export default Navbar;
