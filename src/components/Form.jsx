d96fdd10121411a7d98541928e2f1512c600b8e7
Colin Curtis <colinpcurtis826@ucla.edu>
2021-10-30 17:51:59 -0700

    stash

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+const Form = (props) => {
+    const [sleep, setSleep] = useState("")
+    
+
+    return (
+        <div>
+            <input type="text" />
+
+        </div>
+    )
+}
+
+export default Form;
