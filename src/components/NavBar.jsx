48b7ed19dbbc46cabab7ebd95194983bfd6a66cf
Colin Curtis <colinpcurtis826@ucla.edu>
2021-10-31 19:15:40 -0700

    clean up code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,6 @@ const NavBar = () => {
           </nav>
       </div>
     );
-  };
+}
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
     <App />
   </BrowserRouter>, 
   document.getElementById('root')
