03e7ce62c78a3ed7e02145f8e9c40e2db4147f95
Elvis Li <runqianli@icloud.com>
2021-11-02 13:39:52 -0700

    Started working on LoginPage css
    Need more specific descriptions to adjust style

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
+import './LoginPage.css';
 
 const LoginPage = () => {
-
     return (
-        <div>
-            <h1>Login to Wellness-Me</h1>
-            <p>Don't have an account: Register</p>
-            <Button onClick={(e) =>  window.location.href='/register'}>Register</Button>
-
-            <p>Have an account: Login</p>
-            <Button primary onClick={(e) =>  window.location.href='/login'}>Login</Button>
+        <div className = "at-form">
+            <div class = "at-title">
+                <h1>Login to Wellness-Me</h1>
+            </div>
+            <div class = "at-login">
+                <p>Have an account: Login</p>
+            </div>
+            <div class = "at-auth">
+                <Button primary onClick={(e) =>  window.location.href='/login'}>Login</Button>
+            </div>
+            <div class = "at-sep">
+                <p>Don't have an account: Register</p>
+            </div>    
+            <div class = "at-register">
+                <Button onClick={(e) =>  window.location.href='/register'}>Register</Button>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-// import { Input, Menu } from 'semantic-ui-react'
-
-const Navbar = (props) => {
-  return (
-    <div class="ui four item menu">
-      <a class="active item">A</a>
-      <a class="item">B</a>
-      <a class="item">C</a>
-      <a class="item">D</a>
-    </div>
-  )
+const NavBar = () => {
+    return (
+      <div>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/" style={{marginLeft:"20px"}}>wellness.me</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto" style={{marginRight:"20px"}}>
+                <a class="nav-item nav-link active" href="/form">Daily Check-In</a>
+                <a class="nav-item nav-link" href="#">Analytics</a>
+                <a class="nav-item nav-link" href="#">Settings</a>
+                </div>
+            </div>
+          </nav>
+      </div>
+    );
 }
 
-export default Navbar;
+export default NavBar;
