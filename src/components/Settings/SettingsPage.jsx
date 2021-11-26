64db8cf71efcf97218273eb39248135b48cf8976
Elvis Li <runqianli@icloud.com>
2021-11-25 22:33:01 -0800

    Separate css files
    Separate LoginPage.css from App.css and created SettingsPage.css to do styling.

--- src/components/Settings/SettingsPage.jsx
+++ src/components/Settings/SettingsPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 import Logout from './Logout';
 
 const SettingsPage = () => {
 
     return (
-        <div>
-            <h3 className="greeting">you can use this page to change your user settings</h3>
-
-            <Logout />
+        <div className = "setting-columns" style= {{ display: "flex" }}>
+            <div className = "setting-box" style= {{ flexBasis: "100%", borderColor: "gray", borderStyle: "solid", borderWidth: "1px", borderRadius: "10px", padding: "30px", marginLeft: "30px", marginRight: "30px"}}>
+                <h3 className="greeting">you can use this page to change your user settings</h3>
+                <Logout />
+                <Button>Delete all data</Button>
+            </div>
         </div>
+        
     )
 }
 
