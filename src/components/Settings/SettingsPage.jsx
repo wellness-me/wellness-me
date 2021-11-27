d92adbb04a65ebf8a623e5328d2c2b874da2b017
Elvis Li <runqianli@icloud.com>
2021-11-26 22:43:03 -0800

    Style setting page
    Setting page is styled, version 1

--- src/components/Settings/SettingsPage.jsx
+++ src/components/Settings/SettingsPage.jsx
@@ -6,10 +6,19 @@ const SettingsPage = () => {
 
     return (
         <div className = "setting-columns" style= {{ display: "flex" }}>
-            <div className = "setting-box" style= {{ flexBasis: "100%", borderColor: "gray", borderStyle: "solid", borderWidth: "1px", borderRadius: "10px", padding: "30px", marginLeft: "30px", marginRight: "30px"}}>
-                <h3 className="greeting">you can use this page to change your user settings</h3>
-                <Logout />
-                <Button>Delete all data</Button>
+            <h1 className="greeting">Settings</h1>
+            <div className = "logout-box" style= {{ flexBasis: "100%", borderColor: "gray", borderStyle: "solid", borderWidth: "1px", borderRadius: "10px", padding: "30px", marginLeft: "30px", marginRight: "30px"}}>
+                <div style = {{ display: "flex", justifyContent: "left" }}>
+                    <Logout />
+                </div>
+            </div>
+            <div className = "delete-box" style= {{ flexBasis: "100%", borderColor: "gray", borderStyle: "solid", borderWidth: "1px", borderRadius: "10px", padding: "30px", marginLeft: "30px", marginRight: "30px", marginTop: "50%"}}>
+                <div style = {{ display: "flex"}}>
+                    <Button style={{backgroundColor: 'rgb(209, 26, 42)', color: 'white', justifyContent: "left", fontSize: "90%"}}>DELETE ACCOUNT</Button>
+                    <div style = {{marginLeft: "10%", fontSize: "110%", verticalAlign: "middle", marginTop: "5px"}}>
+                        <p>Are you sure you want to delete all your data stored at wellness.me?</p>
+                    </div>
+                </div>
             </div>
         </div>
         
