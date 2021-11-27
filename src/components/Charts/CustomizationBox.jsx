7fc74c130a7f7c73a0085c351e2555017d0190a7
Colin Curtis <colinpcurtis826@ucla.edu>
2021-11-26 17:55:48 -0800

    remove unworking options in UserCoustomizableCharts in favor of simpler ui

--- src/components/Charts/CustomizationBox.jsx
+++ src/components/Charts/CustomizationBox.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Button } from "semantic-ui-react";
 
-const UNSELECTABLE_PROPS = ['__v', '_id', 'createdAt', 'updatedAt', 'userID']
+const UNSELECTABLE_PROPS = ['__v', '_id', 'createdAt', 'updatedAt', 'userID', "journal"]
 
 const CustomizationBox = (props) => {/* can be converted back into a functional component*/
 
@@ -39,14 +40,10 @@ const CustomizationBox = (props) => {/* can be converted back into a functional 
     if (props.data.length < 1) {
         return (<h5>Please input some data before you can select it</h5>)
     } else {
-        //console.log('the props for customization box are', props);
         const x = props.data[props.data.length - 1]
-        //        console.log('matan!', Object.keys(x));
         const selectableKeys = Object.keys(x).filter((value) => { return !UNSELECTABLE_PROPS.includes(value) })
-        //console.log(selectableKeys);
 
         let toOutputLabels;
-        //console.log('startIndexDate', startDateIndex, props.data.length - 1);
         if (startDateIndex < 0) {
             toOutputLabels = <option key={-1} id={0} value={-1} >Please select the start index first -- the entire range is defualt </option>
         }
@@ -60,49 +57,25 @@ const CustomizationBox = (props) => {/* can be converted back into a functional 
         }
 
 
-
         return (
             <div className="customization-box-wrapper">
                 <div className="select-categories-div">
                     <h4> Select what you would like the chart to display</h4>
-                    <h6>(up to 2)</h6>
                     {
                         selectableKeys.map((value) => {
                             return (
                                 <div className="select-input-container">
-                                    <input onChange={internallyHandleCheckboxChange} class="form-check-input" type="checkbox" key={value} value={value} className="flexCheckDefault" />
-                                    <label>{value}</label>
+                                    <Button primary onClick={internallyHandleCheckboxChange} key={value} value={value}>{value}</Button>
+                                    {/* <input onChange={internallyHandleCheckboxChange} class="form-check-input" type="checkbox" key={value} value={value} className="flexCheckDefault" />
+                                    <label>{value}</label> */}
                                 </div>
                             )
                         })
                     }
-                </div>
-                <div className="select-timerange-div">
-                    <div className="range-start-div">
-                        <h5>When does the range start?(inclusive)</h5>
-                        <select name="time-domain" id="time-domain-from" onChange={handleSelectBoxChangeFrom}>
-                            <option key={0} id={0} value={-1} >None Selected-- the entire range is defualt </option>
-                            {
-                                props.data.map((element, index) => {
-                                    return <option key={element._id} id={element._id} value={index} >{Date(element.createdAt)}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div className="range-start-div">
-                        <h5>When does the range end? (inclusive)</h5>
-                        <select name="time-domain" id="time-domain-to" onChange={handleSelectBoxChangeTo}>
-                            {toOutputLabels}
-                        </select>
-                    </div>
                 </div>
             </div>
         )
     }
 }
 
-
-
-
-
-export default CustomizationBox
+export default CustomizationBox;
