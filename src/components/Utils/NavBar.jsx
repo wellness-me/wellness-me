65b12041ec0dd60b94bf26e8f0f33ff7d9500397
Elvis Li <runqianli@icloud.com>
2021-12-01 00:04:16 -0800

    Bug fixes
    Fix navbar in darkmode

--- src/components/Utils/NavBar.jsx
+++ src/components/Utils/NavBar.jsx
@@ -10,16 +10,16 @@ class NavBar extends React.Component {
     return (
       <Navbar collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="/" style={{marginLeft:"20px"}}>wellness.me</Navbar.Brand>
+          <Navbar.Brand href="/" style={{marginLeft:"20px", color:'rgb(137, 207, 240)'}}>wellness.me</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               {}
             </Nav>
-            <Nav className="nav-button">
-              <Nav.Link href="/form">Daily Check-In</Nav.Link>
-              <Nav.Link href="/analytics">Analytics</Nav.Link>
-              <Nav.Link href="/settings">Settings</Nav.Link>
+            <Nav className="nav-button" >
+              <Nav.Link href="/form" style={{color:'rgb(137, 207, 240)'}}>Daily Check-In</Nav.Link>
+              <Nav.Link href="/analytics" style={{color:'rgb(137, 207, 240)'}}>Analytics</Nav.Link>
+              <Nav.Link href="/settings" style={{color:'rgb(137, 207, 240)'}}>Settings</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
